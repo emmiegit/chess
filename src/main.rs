36004ea81e794eebd8764dcd37e48c0c6a9980c8
Ammon Smith <ammon.i.smith@gmail.com>
2022-04-16 13:19:34 -0400

    Add initial code files.

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+/*
+ * main.rs
+ *
+ * mallard-chess - Chess engine wrapper utility for fun
+ * Copyright (C) 2022 Ammon Smith
+ *
+ * mallard-chess is available free of charge under the terms of the MIT
+ * License. You are free to redistribute and/or modify it under those
+ * terms. It is distributed in the hopes that it will be useful, but
+ * WITHOUT ANY WARRANTY. See the LICENSE file for more details.
+ */
+
+fn main() {
+    todo!();
+}
