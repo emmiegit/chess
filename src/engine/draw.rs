f500bdf045e019ed21cc90a6c4a9db2f5a1bffc5
Ammon Smith <ammon.i.smith@gmail.com>
2022-04-17 16:43:08 -0400

    Implement Drawfish.
    Based on choosing moves which give a minor material advantage.

--- src/engine/draw.rs
+++ src/engine/draw.rs
@@ -11,6 +11,7 @@
  */
 
 use super::prelude::*;
+use crate::score::{Score, ScoredMove};
 
 #[derive(Debug)]
 pub struct DrawfishEngine;
@@ -29,6 +30,49 @@ impl Engine for DrawfishEngine {
     }
 
     fn choose_move(&self, game: &mut Game) -> ChessMove {
-        todo!()
+        let mut moves = game
+            .stockfish
+            .evaluate_possible_moves(&game.board)
+            .into_iter()
+            .map(|ScoredMove { chess_move, score }| {
+                const BIG_VALUE: i32 = 1_000_000_000;
+
+                // If a stalemate is possible, then take it.
+                // If not, choose lowest-rated positive move.
+                // Checkmates of any kind are strongly disincentivized.
+                let new_score = match score {
+                    // Invert material changes.
+                    // If a gain is huge, we want to make it look like a bad choice.
+                    // If a gain is minor, we want to make it look more favorable.
+                    Score::Centipawns(value) if value > 0 => BIG_VALUE / value,
+
+                    // For losses, keep the value as-is.
+                    // We want to prefer minor losses in material to major ones.
+                    Score::Centipawns(value) => value,
+
+                    // For stalemates or checkmates, give a massive fixed score.
+                    //
+                    // We subtract by moves in stalemate since we slightly prefer
+                    // moves that get us to stalemate faster.
+                    Score::Stalemate(moves) => BIG_VALUE - i32::from(moves),
+                    Score::OurMate(_) | Score::TheirMate(_) => -BIG_VALUE,
+                };
+
+                (chess_move, new_score)
+            })
+            .collect::<Vec<_>>();
+
+        // Sort moves by recalculated score
+        moves.sort_by_key(|&(_, score)| score);
+
+        log!(
+            game.log_file,
+            "Scored possible moves for drawfish: {:?}",
+            moves,
+        );
+
+        // Choose the best-scoring move.
+        // Because it sorts from least to greatest, this should be the last one.
+        moves.last().expect("No valid moves").0
     }
 }
